380a8201c311419860e50d71ad5a4cedb40450b8
yegorov-boris <borisyegorov8@gmail.com>
2018-05-26 14:04:18 +0300

    Restart on error

--- mandelbrot-set.go
+++ mandelbrot-set.go
@@ -6,20 +6,32 @@ import (
 	"log"
 	"net"
 	"golang.org/x/net/netutil"
+	"time"
 )
 
 const connectionsCount = 2
+const sleepBeforeRestart = 5 * time.Second
 
 func main() {
+	startServer()
+}
+
+func startServer() {
 	l, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatalf("Listen: %v", err)
+		log.Fatalf("Listen: %v\n", err)
 	}
 	l = netutil.LimitListener(l, connectionsCount)
 	defer l.Close()
 
 	http.HandleFunc("/", handler)
-	log.Fatal(http.Serve(l, nil))
+	serverErr := http.Serve(l, nil)
+	if serverErr != nil {
+		log.Printf("Server failed: %v\n", serverErr)
+		log.Println("Restarting")
+		time.Sleep(sleepBeforeRestart)
+		startServer()
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
